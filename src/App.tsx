035d0a772c825a67c12ad3e48702e1e1b3801958
Xavier <152043254+Xavier-L-25@users.noreply.github.com>
2024-07-03 19:05:55 -0700

    Basic navbar layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,20 @@ import "./App.css";
 
 function App() {
   return (
-    <>
+    <div className="container mx-auto">
+      <nav className="navbar mt-4">
+        <text className="navbar-start text-3xl font-extrabold">
+          Xavier Lopez
+        </text>
+        <text className="navbar-end text-xl font-semibold">About</text>
+        <text className="navbar-end text-xl font-semibold">Skills</text>
+        <text className="navbar-end text-xl font-semibold">Contact</text>
+        <text className="navbar-end text-xl font-semibold">Resume</text>
+      </nav>
       <h1 className="text-3xl font-bold underline text-red-500">
         Hello world!
       </h1>
-    </>
+    </div>
   );
 }
 
